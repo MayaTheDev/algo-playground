be8d8bcf87cab45eb585f32717deda13dde959ed
Maya <hello@mayathedev.com>
2026-02-06 20:17:11 +0100

    Add insertion sort

---
+++ src/algorithms/insertion-sort/insertion-sort.logic.ts
@@ -0,0 +1,27 @@
+import type { SortStep } from '../../types/algo.types'
+
+export function* insertionSortSteps(input: number[]): Generator<SortStep> {
+  const arr = [...input]
+  const n = arr.length
+  const sorted = new Set<number>([0])
+
+  yield { array: [...arr], comparing: null, sorted: [...sorted], activeRange: null }
+
+  for (let i = 1; i < n; i++) {
+    let j = i
+
+    while (j > 0) {
+      yield { array: [...arr], comparing: [j - 1, j], sorted: [...sorted], activeRange: null }
+
+      if (arr[j] < arr[j - 1]) {
+        ;[arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+        j--
+      } else {
+        break
+      }
+    }
+
+    sorted.add(i)
+    yield { array: [...arr], comparing: null, sorted: [...sorted], activeRange: null }
+  }
+}
