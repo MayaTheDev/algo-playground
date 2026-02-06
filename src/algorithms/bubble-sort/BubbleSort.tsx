5426df5bd383595ed8565f83b88acdbe3430e75e
Maya <hello@mayathedev.com>
2026-02-05 20:28:44 -0800

    Add bubble sort

---
+++ src/algorithms/bubble-sort/BubbleSort.tsx
@@ -0,0 +1,28 @@
+import { useState, useMemo } from 'react'
+import { SortBars } from '../../components/SortBars'
+import { Controls } from '../../components/Controls'
+import { useAlgoPlayer } from '../../hooks/useAlgoPlayer'
+import { generateRandomArray, collectSteps } from '../../utils/array.utils'
+import { bubbleSortSteps } from './bubble-sort.logic'
+
+export function BubbleSort() {
+  const [input, setInput] = useState(() => generateRandomArray(16))
+  const steps = useMemo(() => collectSteps(bubbleSortSteps(input)), [input])
+  const player = useAlgoPlayer(steps)
+
+  const handleNewInput = () => {
+    player.reset()
+    setInput(generateRandomArray(16))
+  }
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-1 flex items-center justify-center p-4">
+        <div className="w-full max-w-2xl">
+          <SortBars step={player.currentStep} />
+        </div>
+      </div>
+      <Controls player={player} onNewInput={handleNewInput} />
+    </div>
+  )
+}
